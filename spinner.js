baa90bc1d1a2ea13bbbdb5fd4ab88b99df38d43e
ZakiyaAlfughi <47117848+ZakiyaA@users.noreply.github.com>
2021-11-30 23:46:38 -0500

    Create spinner.js

---
+++ spinner.js
@@ -0,0 +1,25 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1000);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 3000);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 5000);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 7000);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 8000);
+
+setTimeout(() => {
+  process.stdout.write('\n   ');
+}, 9000);
